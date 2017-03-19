bc535c453eae25a9149a7cc4860838666be84e36
enhoshen <enhoshen@gmail.com>
2017-03-19 21:58:43 +0800

    Update embd.sh

--- hw1/embd.sh
+++ hw1/embd.sh
@@ -1,2 +1,2 @@
-wget -O embd.rar https://www.dropbox.com/sh/nokgjr5hwxp8qyt/AADHNH9-_ri5373d_p2YG7cJa?dl=1
+wget -O ./embd.rar https://www.dropbox.com/sh/nokgjr5hwxp8qyt/AADHNH9-_ri5373d_p2YG7cJa?dl=1
 unrar -x embd.rar 
