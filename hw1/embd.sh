be54333dd93e441bda45a525177c15ff06e73f56
enhoshen <enhoshen@gmail.com>
2017-03-19 22:45:38 +0800

    Update embd.sh

--- hw1/embd.sh
+++ hw1/embd.sh
@@ -1,2 +1,6 @@
-wget -O embd.rar https://www.dropbox.com/sh/nokgjr5hwxp8qyt/AADHNH9-_ri5373d_p2YG7cJa?dl=1
-unrar x embd.rar 
+wget -O embd.zip https://www.dropbox.com/s/s54agpg1k0yahc3/embd.zip?dl=1
+unzip embd.zip 
+cd ./embd
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+g++ -std=c++11 -shared word2vec_ops.cc word2vec_kernels.cc -o word2vec_ops.so -fPIC -I $TF_INC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+cd ..
