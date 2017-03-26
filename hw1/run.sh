6484102017cffc0db6013c89a167dc53263d1348
johnson <john820911@users.noreply.github.com>
2017-03-26 23:31:39 +0800

    directly change from browser

--- hw1/run.sh
+++
@@ -1,6 +0,0 @@
-savezip = save.zip
-wget -O savefile
-unzip -u-o savefile
-#get the saved model
-python cloze_prediction.py 0 $1 $2 hand_in 
-#0 means don't reparse training data,$1 is path to testing data , $2 is output data path
