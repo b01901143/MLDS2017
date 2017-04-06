0b61db8ceba63200b35f40688a5fd021b24deeeb
Johnson Jiang <johnsonjiang@JohnsondeMacBook-Pro.local>
2017-04-06 10:35:31 +0800

    organize

--- hw1/embd.sh
+++
@@ -1,3 +0,0 @@
-wget -O embd.zip https://www.dropbox.com/s/geq1kk0fmniqk38/embd.zip?dl=1
-unzip -u-o embd.zip
-rm embd.zip
--- hw1/run.sh
+++ hw1/run.sh
@@ -1,3 +1,5 @@
-cd baseline/
-sh run.sh $1 $2
-cd ..
+wget -O save.zip https://www.dropbox.com/s/xviu2iqbukhmn4f/save.zip?dl=1
+wget -O data.zip https://www.dropbox.com/s/heacx5oy3ae43pl/data.zip?dl=1
+unzip -u-o save.zip
+unzip -u-o data.zip
+python test.py $1 $2  
