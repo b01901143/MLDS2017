f1310a1f19fd03776c89e656140b5845fa8407a5
john820911 <r05943135@ntu.edu.tw>
2017-04-18 12:37:13 +0800

    parameter.py

--- hw2/parameter.py
+++ hw2/parameter.py
@@ -1,17 +1,24 @@
+#parameter
 video_size = 4096
 video_step = 80
 caption_size = 1932
 caption_step = 12
 hidden_size = 256
-num_epoch = 500
 batch_size = 10
+num_epoch = 500
+save_per_epoch = 10
 learning_rate = 0.001
 
+#for other settings
+per_process_gpu_memory_fraction = 0.8
+max_to_keep=10 #default 5 recent checkpoints
+
+#path
 train_feat_dir = "./data/training/feat/"
 train_info_path = "./data/training/info.csv"
 test_feat_dir = "./data/testing/feat/"
 test_info_path = "./data/testing/info.csv"
-model_path = "_".join((
+model_path = "model/" + "_".join((
         "num_vocabulary", str(caption_size),
         "hidden_size", str(hidden_size),
         "num_epoch", str(num_epoch),
