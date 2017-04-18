13c6ef1f31b5dd1f397f9bd46b20d75521d52070
john820911 <r05943135@ntu.edu.tw>
2017-04-18 17:33:14 +0800

    train.py

--- hw2/train.py
+++ hw2/train.py
@@ -22,77 +22,60 @@ class VideoCaptionGenerator():
         self.video_encode_W = tf.Variable(tf.random_uniform([video_size, hidden_size], -0.1, 0.1), name="video_encode_W")
         self.video_encode_b = tf.Variable(tf.zeros([hidden_size]), name="video_encode_b")        
         #lstm layers
-        self.lstm1 = tf.contrib.rnn.BasicLSTMCell(hidden_size, state_is_tuple=False)
-        self.lstm2 = tf.contrib.rnn.BasicLSTMCell(hidden_size, state_is_tuple=False)
+        self.lstm_1 = tf.contrib.rnn.BasicLSTMCell(hidden_size, state_is_tuple=False)
+        self.lstm_2 = tf.contrib.rnn.BasicLSTMCell(hidden_size, state_is_tuple=False)
         #decode layers
         self.caption_decode_W = tf.Variable(tf.random_uniform([hidden_size, caption_size], -0.1, 0.1), name="caption_decode_W")
         self.caption_decode_b = tf.Variable(tf.zeros([caption_size]), name="caption_decode_b")
     def buildModel(self):
-        #placeholder
+        #placeholders
         tf_video_array = tf.placeholder(tf.float32, [self.batch_size, self.video_step, self.video_size])
         tf_video_array_mask = tf.placeholder(tf.float32, [self.batch_size, self.video_step])
         tf_caption_array = tf.placeholder(tf.int32, [self.batch_size, self.caption_step])
         tf_caption_array_mask = tf.placeholder(tf.float32, [self.batch_size, self.caption_step])
-
-        video_flat = tf.reshape(video, [-1, self.video_size])
-        image_emb = tf.nn.xw_plus_b( video_flat, self.encode_image_W, self.encode_image_b )
-        image_emb = tf.reshape(image_emb, [self.batch_size, self.video_step, self.hidden_size])
-
-        state1 = tf.zeros([self.batch_size, self.lstm1.state_size])
-        state2 = tf.zeros([self.batch_size, self.lstm2.state_size])
+        #tensors
+        video_array_flat = tf.reshape(tf_video_array, [-1, self.video_size])
+        video_embed = tf.nn.xw_plus_b(video_array_flat, self.video_encode_W, self.video_encode_b)
+        video_embed = tf.reshape(video_embed, [self.batch_size, self.video_step, self.hidden_size])
+        state_1 = tf.zeros([self.batch_size, self.lstm_1.state_size])
+        state_2 = tf.zeros([self.batch_size, self.lstm_2.state_size])
         padding = tf.zeros([self.batch_size, self.hidden_size])
-        probs = []
-        loss = 0.0
-
-        with tf.variable_scope("RNN"):
-
-            ##############################  Encoding Stage ##################################
-            for i in range(0, self.video_step):
+        tf_loss = 0.0
+        with tf.varaible_scope("RNN"):
+            #encoding stage
+            for step in range(self.video_step):
                 if i > 0:
                     tf.get_variable_scope().reuse_variables()
-
                 with tf.variable_scope("LSTM1"):
-                    output1, state1 = self.lstm1(image_emb[:,i,:], state1)
-
+                    output_1, state_1 = self.lstm_1(video_embed[:, i, :], state_1)
                 with tf.variable_scope("LSTM2"):
-                    output2, state2 = self.lstm2(tf.concat([padding, output1], 1), state2)
-
-            ############################# Decoding Stage ######################################
-            for i in range(0, self.caption_step-1): ## Phase 2 => only generate captions
-                #if i == 0:
-                #    current_embed = tf.zeros([self.batch_size, self.hidden_size])
-                #else:
+                    output_2, state_2 = self.lstm_2(tf.concat([padding, output_1], 1), state_2)
+            #decoding stage
+            for i in range(self.caption_step-1):
                 with tf.device("/cpu:0"):
-                    current_embed = tf.nn.embedding_lookup(self.Wemb, caption[:, i])
-
+                    caption_embed = tf.nn.embedding_lookup(self.caption_encode_W, tf.caption_array[:, i])
                 tf.get_variable_scope().reuse_variables()
-
                 with tf.variable_scope("LSTM1"):
-                    output1, state1 = self.lstm1(padding, state1)
-
+                    output_1, state_1 = self.lstm_1(padding, state_1)
                 with tf.variable_scope("LSTM2"):
-                    output2, state2 = self.lstm2(tf.concat([current_embed, output1], 1), state2)
-
-                labels = tf.expand_dims(caption[:, i+1], 1)
-                indices = tf.expand_dims(tf.range(0, self.batch_size, 1), 1)
-                concated = tf.concat([indices, labels], 1)
-                onehot_labels = tf.sparse_to_dense(concated, tf.stack([self.batch_size, self.caption_size]), 1.0, 0.0)
-
-                logit_words = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
-                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logit_words, labels = onehot_labels)
-                cross_entropy = cross_entropy * caption_mask[:,i]
-                probs.append(logit_words)
-
-                current_loss = tf.reduce_sum(cross_entropy)/self.batch_size
-                loss = loss + current_loss
-        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-        return loss, video, video_mask, caption, caption_mask, probs
-
+                    output_2, state_2 = self.lstm_2(tf.concat([caption_embed, output_1], 1), state_2)
+                #cross entropy
+                logits = tf.nn.xw_plus_b(output2, self.caption_decode_W, self.caption_decode_b)
+                labels = tf.sparse_to_dense(
+                    tf.concat([tf.expand_dims(tf.range(self.batch_size), 1), tf.expand_dims(tf_caption_array[:, i+1], 1)], 1),
+                    tf.stack([self.batch_size, self.caption_size]),
+                    1.0,
+                    0.0
+                )
+                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels) * caption_array_mask[:, i]
+                tf_loss += tf.reduce_sum(cross_entropy) / self.batch_size
+        tf_optimizer = tf.train.AdamOptimizer(learning_rate).minimize(tf_loss)     
+        return tf_video_array, tf_video_array_mask, tf_caption_array, tf_caption_array_mask, tf_loss, tf_optimizer
 
     def buildGenerator(self):
         
         # tf.get_variable_scope().reuse_variables()
-
+        
         video = tf.placeholder(tf.float32, [1, self.video_step, self.video_size])
 
         video_flat = tf.reshape(video, [-1, self.video_size])
