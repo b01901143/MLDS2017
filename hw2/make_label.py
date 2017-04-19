ca12a810942441bd9e55085829e9423f37cac7df
john820911 <r05943135@ntu.edu.tw>
2017-04-19 16:31:37 +0800

    fix

--- hw2/make_label.py
+++ hw2/make_label.py
@@ -6,27 +6,31 @@ import codecs
 
 #argv
 label_json_path = "./data/" + sys.argv[1] + "/label.json"
+label_dir = "./data/" + sys.argv[1] + "/label/"
 info_csv_path = "./data/" + sys.argv[1] + "/info.csv"
-sentence_length = sys.argv[2] #min_mutual_sentence_length = 9
+sentence_length = sys.argv[2] #min_mutual_sentence_length = 8, max_mutual_sentence_length = 40
 
 #open
 output_label = json.load(codecs.open(label_json_path, "r", "utf-8"))
 
-#select
+#preprocess
 for label in output_label:
 	temp_sentences = [ nltk.word_tokenize(sentence) for sentence in label["caption"] ]
-	temp_strip_sentences = [ sentence for sentence in temp_sentences if(len(sentence) <= int(sentence_length)) ]
-	temp_sorted_sentences = sorted(temp_strip_sentences, key=len)
-	label["caption"] = temp_sorted_sentences[-1]
+	punct_word_list = [ ",", ".", "!", "?", " " ]
+	temp_regex_sentences = [ [  word for word in sentence if word not in punct_word_list ] for sentence in temp_sentences ]
+	temp_strip_sentences = [ sentence for sentence in temp_regex_sentences if(len(sentence) <= int(sentence_length)) ]
+	temp_join_sentences = [ " ".join(sentence) for sentence in temp_strip_sentences ]
+	with open(label_dir + label["id"] + ".json", "w") as label_file:
+		json.dump(temp_join_sentences, label_file, indent=4, sort_keys=True) 
 
 #make_id_caption_dict
-id_caption_list_of_list = [ [ label["id"] + ".npy", " ".join(label["caption"]) ] for label in output_label ]
-id_caption_list_of_list.insert(0, ["feat_path", "label_sentence"])
+id_caption_list_of_list = [ [ label["id"] + ".npy", label["id"] + ".json" ] for label in output_label ]
+id_caption_list_of_list.insert(0, ["feat_path", "label_path"])
 
 #convert to utf-8
 id_caption_list_of_list = [ [ unicode(w).encode("utf-8") for w in id_caption_list ] for id_caption_list in id_caption_list_of_list ]
 
 #output
-with open(info_csv_path, "w") as label_file:
-	writer = csv.writer(label_file)
+with open(info_csv_path, "w") as info_file:
+	writer = csv.writer(info_file)
 	writer.writerows(id_caption_list_of_list)
