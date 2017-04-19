65c8c7b590e1a12804823207d5e07c0ee63b5a68
john820911 <r05943135@ntu.edu.tw>
2017-04-19 13:49:00 +0800

    fix

---
+++ hw2/download_data.sh
@@ -0,0 +1,2 @@
+wget -O data.tar.gz https://www.dropbox.com/s/vwkh4n4rqvvgsaw/data.tar.gz?dl=1
+unzip -u-o data.tar.gz
---
+++ hw2/download_glove.sh
@@ -0,0 +1,2 @@
+wget -O glove_42B.zip http://nlp.stanford.edu/data/glove.42B.300d.zip
+unzip -u-o glove_42B.zip
