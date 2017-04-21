271631dc2f8256de517ee4d25e247148108933b9
enhoshen <enhoshen@media.ee.ntu.edu.tw>
2017-04-21 19:50:06 +0800

    update caption encode_W

--- hw2/DEJ/structure.py
+++ hw2/DEJ/structure.py
@@ -13,7 +13,10 @@ class VideoCaptionGenerator():
         self.batch_size = batch_size
         #encode layers
         with tf.device("/cpu:0"):
-            self.caption_encode_W = tf.Variable(tf.random_uniform([caption_size, hidden_size], -0.1, 0.1), name="caption_encode_W")
+            if Embd_flag is False:
+                self.caption_encode_W = tf.Variable(tf.random_uniform([caption_size, hidden_size], -0.1, 0.1), name="caption_encode_W")
+            else :
+                self.caption_encode_W = tf.Variable(pretrained_embd,name="caption_encode_W")
         self.video_encode_W = tf.Variable(tf.random_uniform([video_size, hidden_size], -0.1, 0.1), name="video_encode_W")
         self.video_encode_b = tf.Variable(tf.zeros([hidden_size]), name="video_encode_b")        
         #lstm layers
