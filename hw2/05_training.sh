acbd7558cfd541f86e9fb5522a93f280054a3687
john820911 <r05943135@ntu.edu.tw>
2017-04-27 03:45:35 +0800

    remove train/test.sh (train/test inside directories)

--- hw2/05_training.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Train VCG model..."
-
-model="D" #D/D_ATTEN/DES/DEJ four types of models
-path=$model"/"train.py
-
-python $path 
-
-echo "Finish training!!"
--- hw2/06_testing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Test VCG model..."
-
-model="D" #D/D_ATTEN/DES/DEJ four types of models
-path=$model"/"test.py
-
-python $path 
-
-echo "Finish testing!!"
