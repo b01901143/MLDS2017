86d426acaf731345bc689f49a09c39986a1835b3
john820911 <r05943135@ntu.edu.tw>
2017-04-27 02:46:42 +0800

    rename

--- hw2/03_build_info.sh
+++ hw2/03_build_info.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Preprocess data..."
+echo "Preprocess info..."
 
 taken="one" #one/all
 bound=10 #8~40
 
 for type in "training" "testing"; do
 	python preprocess.py $type $taken $bound
-	echo "Finish Preprocessing" $type "data!!" 
+	echo "Finish Preprocessing" $type "info!!" 
 done
