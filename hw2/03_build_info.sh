61da674e67df2069737eca6c4c4367a3bef94063
xzero000 <b01901063@ntu.edu.tw>
2017-04-27 09:03:02 +0800

    add trash on previous version,fix

--- hw2/03_build_info.sh
+++ hw2/03_build_info.sh
@@ -5,6 +5,6 @@ taken="all" #one/all
 bound=40 #8~40
 
 for type in "training" "testing"; do
-	python3 build_info.py $type $taken $bound
+	python build_info.py $type $taken $bound
 	echo "Finish building" $type "info!!" 
 done
