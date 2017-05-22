f0e7a86c1a48f44ce9cbc83388752fde5f8ba68a
Johnson Jiang <r05943135@ntu.edu.tw>
2017-05-23 00:10:32 +0800

    modified

--- hw3/03_sample_training_text.sh
+++ hw3/03_sample_training_text.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Prepare captions..."
+echo "Sample training text..."
 
 INFO_DIR="info"
 if [ ! -d "$INFO_DIR" ]; then
@@ -10,6 +10,6 @@ infile_path="./data/basic/tags.csv"
 outfile_path="./$INFO_DIR/sample_training_text.txt"
 feature_type="hair eyes"
 
-python prepare_captions.py --infile_path "$infile_path" --outfile_path "$outfile_path" --feature_type "$feature_type"
+python sample_training_text.py --infile_path "$infile_path" --outfile_path "$outfile_path" --feature_type "$feature_type"
 
-echo "Finish preparing captions!!"
+echo "Finish sampling training text!!"
