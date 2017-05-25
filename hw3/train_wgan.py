ac278feb0a2e4ef1df16cfec976081e62421e27d
Johnson Jiang <r05943135@ntu.edu.tw>
2017-05-25 18:02:36 +0800

    fix

--- hw3/train_wgan.py
+++ hw3/train_wgan.py
@@ -8,7 +8,6 @@ from model_wgan import *
 
 def train():
 	#get text_image info
-	sample_training_text_dict = readSampleText(sample_training_text_path)
 	sample_training_info_list = readSampleInfo(sample_training_info_path)
 	#model
 	model = GAN(
@@ -24,10 +23,8 @@ def train():
 	with tf.variable_scope("GAN"):
 		input_tensor, output_tensor, loss_tensor, variable_tensor, check_tensor = model.buildTrainModel()
 	#optimizer
-	g_optimizer = tf.train.RMSPropOptimizer(learning_rate = 0.0001).minimize(loss_tensor['g_loss'], var_list=variable_tensor['g_variable'])
-	#g_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(loss_tensor['g_loss'], var_list=variable_tensor['g_variable'])
-	d_optimizer = tf.train.AdamOptimizer(learning_rate = 0.0001).minimize(loss_tensor['d_loss'], var_list=variable_tensor['d_variable'])
-	#d_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(loss_tensor['d_loss'], var_list=variable_tensor['d_variable'])
+	g_optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss_tensor['g_loss'], var_list=variable_tensor['g_variable'])
+	d_optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss_tensor['d_loss'], var_list=variable_tensor['d_variable'])
 	if d_optimizer >= 0.01:
 		d_optimizer = 0.01
 	if d_optimizer <= -0.01:
@@ -35,7 +32,6 @@ def train():
 	#session and saver
 	session = tf.InteractiveSession()
 	saver = tf.train.Saver()
-	saver_50=tf.train.Saver()
 	if restore_flag:
 		saver.restore(session, model_dir + "-" + str(restore_version))
 	else:
@@ -52,10 +48,10 @@ def train():
 			current_train_data = sample_training_info_list[batch * batch_size : (batch+1) * batch_size]
 			real_image, wrong_image, caption, noise, image_file = getTrainData(current_train_data)
 			feed_dict = {
-				input_tensor['real_image']: real_image,
-				input_tensor['wrong_image']: wrong_image,
-				input_tensor['caption']: caption,
-				input_tensor['noise']: noise
+				input_tensor["real_image"]: real_image,
+				input_tensor["wrong_image"]: wrong_image,
+				input_tensor["caption"]: caption,
+				input_tensor["noise"]: noise
 			}			
 			g_fetch_dict = {
 				"g_optimizer": g_optimizer,
@@ -65,10 +61,10 @@ def train():
 			d_fetch_dict = {
 				"d_optimizer": d_optimizer,
 				"d_loss": loss_tensor["d_loss"],
-				"fake_image": output_tensor['fake_image'],
-				"d_loss_1": check_tensor['d_loss_1'],
-				"d_loss_2": check_tensor['d_loss_2'],
-				"d_loss_3": check_tensor['d_loss_3']
+				"fake_image": output_tensor["fake_image"],
+				"d_loss_1": check_tensor["d_loss_1"],
+				"d_loss_2": check_tensor["d_loss_2"],
+				"d_loss_3": check_tensor["d_loss_3"]
 			}
 			d_track_dict = session.run(d_fetch_dict, feed_dict=feed_dict)
 			g_track_dict = session.run(g_fetch_dict, feed_dict=feed_dict)
@@ -79,10 +75,23 @@ def train():
 				for f in os.listdir(result_training_dir):
 					file_path = os.path.join(result_training_dir, f)
 					os.unlink(file_path)
-				saveImageCaption(result_training_dir, result_training_caption_path, sample_training_text_dict, g_track_dict["fake_image"], image_file)	
+				with open(result_caption_path, "wb") as result_training_caption_file:
+					writer = csv.writer(result_training_caption_file)	
+					list_ = []
+					for i, (image, file) in enumerate(zip(g_track_dict["fake_image"], image_file)):
+						saved_image = np.zeros((image_size, image_size, 3), dtype=np.uint8)
+						saved_image = image
+						id_ = file.split("/")[-1].split(".")[0]
+						caption = sample_training_text_dict[id_]
+						list_.append((int(id_), (caption, saved_image)))
+					sorted(list_, key=lambda x: list_[0])
+					for id_, (caption, saved_image) in list_:
+						scipy.misc.imsave(result_dir + str(id_) + ".jpg", saved_image)
+						writer.writerow([id_, caption])	
 		sys.stdout.write("\nEpochID: {0}, Saving Model...\n".format(epoch))
 		saver.save(session, model_dir, global_step=epoch)
-		if (batch % 50) == 0 :
-			saver_50.save(session, model_dir, global_step=epoch)
+		if (epoch % save_num_epoch) == 0 :
+			saver.save(session, model_dir, global_step=epoch)
+
 if __name__ == '__main__':
 	train()
