1a001654299f559241a04be001fb3367df110bf0
enhoshen <enhoshen@gmail.com>
2017-06-02 07:55:04 -0400

    Train.py and generate.py for testing and training seperately : 1st edition

--- hw4/parameter.py
+++ hw4/parameter.py
@@ -1,7 +1,7 @@
 #  Generator  Hyper-parameters
 ######################################################################################
-EMB_DIM = 32 # embedding dimension
-HIDDEN_DIM = 32 # hidden state dimension of lstm cell
+EMB_DIM = 300 # embedding dimension
+HIDDEN_DIM = 300 # hidden state dimension of lstm cell
 SEQ_LENGTH = 20 # sequence length
 START_TOKEN = 0
 PRE_EPOCH_NUM = 120 # supervise (maximum likelihood estimation) epochs
@@ -11,7 +11,7 @@ BATCH_SIZE = 64
 #########################################################################################
 #  Discriminator  Hyper-parameters
 #########################################################################################
-dis_embedding_dim = 64
+dis_embedding_dim = 600
 dis_filter_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
 dis_num_filters = [100, 200, 200, 200, 200, 100, 100, 100, 100, 100, 160, 160]
 dis_dropout_keep_prob = 0.75
@@ -27,54 +27,9 @@ negative_file = 'save/generator_sample.txt'
 eval_file = 'save/eval_file.txt'
 generated_num = 10000
 
-#optimizer
-learning_rate = 0.0002
-beta1 = 0.5
-
 #train
 restore_flag = False
-restore_version = 595
 num_epoch = 600
 save_num_batch = 50
-save_num_epoch = 10
 max_to_keep = 100
 
-#path
-'''
-sample_training_info_path = "./info/sample_training_info"
-sample_testing_info_path = "./info/sample_testing_info"
-model_dir = "./models/" + "_".join((
-		"image_size", str(image_size),
-		"caption_size", str(caption_size),
-		"embedding_size", str(embedding_size),
-		"noise_size", str(noise_size),
-		"g_channel_size", str(g_channel_size),
-		"d_channel_size", str(d_channel_size),
-		"batch_size", str(batch_size),
-		"learning_rate", str(learning_rate),
-		"beta1", str(beta1)
-    )) + "/"
-result_training_dir = "./results/training/" + "_".join((
-		"image_size", str(image_size),
-		"caption_size", str(caption_size),
-		"embedding_size", str(embedding_size),
-		"noise_size", str(noise_size),
-		"g_channel_size", str(g_channel_size),
-		"d_channel_size", str(d_channel_size),
-		"batch_size", str(batch_size),
-		"learning_rate", str(learning_rate),
-		"beta1", str(beta1)
-    )) + "/"
-result_testing_dir = "./results/testing/" + "_".join((
-		"image_size", str(image_size),
-		"caption_size", str(caption_size),
-		"embedding_size", str(embedding_size),
-		"noise_size", str(noise_size),
-		"g_channel_size", str(g_channel_size),
-		"d_channel_size", str(d_channel_size),
-		"batch_size", str(batch_size),
-		"learning_rate", str(learning_rate),
-		"beta1", str(beta1)
-    )) + "/"
-sample_dir = "./samples/"
-'''
--- hw4/train.py
+++ hw4/train.py
@@ -7,61 +7,53 @@ from dataloader import Gen_Data_loader, Dis_dataloader
 from generator import Generator
 from discriminator import Discriminator
 from rollout import ROLLOUT
+from function import *
 
+
+model_name="ver1"
 def train():
-	#get text_image info
-	sample_training_info_list = readSampleInfo(sample_training_info_path)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--model_name", type=str, help="for example: seqGan1")
+	parser.add_argument("--restore_version", type=str, help="for example: 300 ")
+	parser.add_argument("--input_file", type=str, help="for example: ./data/training_question.txt")
+	parser.add_argument("--output_file",type=str, help="for example: ./data/training_answer.txt")
+	args = parser.parse_args()
 	#model
-	model = GAN(
-		image_size=image_size,
-		caption_size=caption_size,
-		embedding_size=embedding_size,
-		noise_size=noise_size,
-		g_channel_size=g_channel_size,
-		d_channel_size=d_channel_size,
-		batch_size=batch_size
-	)
-	#build train model
-	with tf.variable_scope("GAN"):
-		input_tensor, output_tensor, loss_tensor, variable_tensor, check_tensor = model.buildTrainModel()
-	#optimizer
-	g_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(loss_tensor['g_loss'], var_list=variable_tensor['g_variable'])
-	d_optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(loss_tensor['d_loss'], var_list=variable_tensor['d_variable'])
-
-	#makedirs
-	if not os.path.exists(model_dir):
-		os.makedirs(model_dir)
-	if not os.path.exists(result_training_dir):
-		os.makedirs(result_training_dir)
-	random.seed(SEED)
-    np.random.seed(SEED)
-    assert START_TOKEN == 0
-
-    gen_data_loader = Gen_Data_loader(BATCH_SIZE)
-    likelihood_data_loader = Gen_Data_loader(BATCH_SIZE) # For testing
-    vocab_size = 5000
-    dis_data_loader = Dis_dataloader(BATCH_SIZE)
-
-    generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN)
-    target_params = cPickle.load(open('save/target_params.pkl'))
-    target_lstm = TARGET_LSTM(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN, target_params) # The oracle model
-
-    discriminator = Discriminator(sequence_length=20, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim, 
+	generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN)
+	discriminator = Discriminator(sequence_length=20, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim, 
                                 filter_sizes=dis_filter_sizes, num_filters=dis_num_filters, l2_reg_lambda=dis_l2_reg_lambda)
 	rollout = ROLLOUT(generator, 0.8)
+
+	
+	#makedirs
+	
+	if not os.path.exists(model_dir+model_name):
+		os.makedirs(model_dir+model_name)
+	if not os.path.exists(sample_dir+model_name):
+		os.makedirs(sample_dir+model_name)
+	random.seed(SEED)
+	np.random.seed(SEED)
+	assert START_TOKEN == 0
+
+	gen_data_loader = Gen_Data_loader(BATCH_SIZE)
+	likelihood_data_loader = Gen_Data_loader(BATCH_SIZE) # For testing
+	vocab_size = 80000
+	dis_data_loader = Dis_dataloader(BATCH_SIZE)
+
+
 	
 	#session and saver
 	session = tf.InteractiveSession()
 	saver = tf.train.Saver()
 	if restore_flag:
-		saver.restore(session, model_dir + "-" + str(restore_version))
+		saver.restore(session, model_dir +model_name + "/-" + restore_version)
 	else:
 		session.run(tf.global_variables_initializer())
-
+	
     
 
-    print '#########################################################################'
-    print 'Start Adversarial Training...'
+	print '#########################################################################'
+	print 'Start Adversarial Training...'
 	for epoch in range(num_epoch):	
 		for batch in range(len(sample_training_info_list)//batch_size):
 			# Train the generator for one step
@@ -100,9 +92,8 @@ def train():
 						_ = sess.run(discriminator.train_op, feed)
 	
 			sys.stdout.write("\nEpochID: {0}, Saving Model...\n".format(epoch))
-			saver.save(session, model_dir, global_step=epoch)
-			if (epoch % save_num_epoch) == 0 :
-				saver.save(session, model_dir, global_step=epoch)
+			saver.save(session, model_dir+model_name, global_step=epoch)
+
 
 if __name__ == '__main__':
 	train()
