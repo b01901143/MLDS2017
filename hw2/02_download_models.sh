45b0bd6615e534d54848088ab8584d22a70124bf
Johnson Jiang <r05943135@ntu.edu.tw>
2017-09-04 21:53:14 +0800

    fix

---
+++ hw2/02_download_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Download models and unzip them..."
+
+echo "Please download models and unzip them manually. The following is the url: https://drive.google.com/file/d/0B2hNk0_VowQmLTB1ZzZfNElPSVE/view?usp=sharing"
+
+echo "Finish downloading models!!"
