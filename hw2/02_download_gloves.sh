72fcc87cf4f77408cb3b414074fd31f914169ec8
Johnson Jiang <r05943135@ntu.edu.tw>
2017-09-04 21:58:41 +0800

    fix

---
+++ hw2/02_download_gloves.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Download gloves and unzip them..."
+
+echo "Please download gloves and unzip them manually. The following is the url: https://drive.google.com/file/d/0B2hNk0_VowQmSUNwWEpSQ1VXNTA/view?usp=sharing"
+
+echo "Finish downloading gloves!!"
